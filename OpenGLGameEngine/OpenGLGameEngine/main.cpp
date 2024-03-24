aa0b80082ddb9a887e1b3185b720771f8a43bca0
btuhany <114235184+btuhany@users.noreply.github.com>
2024-03-24 14:01:24 +0300

    +projection matrix, index draws

--- OpenGLGameEngine/OpenGLGameEngine/main.cpp
+++ OpenGLGameEngine/OpenGLGameEngine/main.cpp
@@ -13,7 +13,7 @@
 const GLint WIDTH = 800, HEIGHT = 600;
 const float toRadians = 3.14159265f / 180.0f;
 
-GLuint VAO, VBO, shader, uniformModel;
+GLuint VAO, VBO, IBO, shader, uniformModel, uniformProjection;
 
 bool direction = true;
 float triOffset = 0.0f;
@@ -32,12 +32,13 @@ static const char* vShader = "										\n\
 layout (location = 0) in vec3 pos;									\n\
 																	\n\
 uniform mat4 model;													\n\
+uniform mat4 projection;											\n\
 																	\n\
 																	\n\
 out vec4 vCol;														\n\
 void main()															\n\
 {																	\n\
-	gl_Position = model * vec4( pos.x, pos.y, pos.z, 1.0);			\n\
+	gl_Position = projection * model * vec4(pos.x, pos.y, pos.z, 1.0);			\n\
 	vCol = vec4(clamp(pos, 0.0, 1.0), 1.0f);						\n\
 }																	\n\
 ";
@@ -56,9 +57,17 @@ void main()									\n\
 ";
 void CreateTriangle()
 {
+	unsigned int indices[] = {
+		0, 3, 1,
+		1, 3, 2,
+		2, 3, 0,
+		0, 1, 2
+	};
+
 	GLfloat vertices[] =
 	{
 		-1.0f, -1.0f, 0.0f,
+		-1.0f, -1.0f, 1.0f,
 		1.0f, -1.0f, 0.0f,
 		0.0f, 1.0f, 0.0f
 	};
@@ -69,6 +78,10 @@ void CreateTriangle()
 	//Bind the VAO and Any opengl functions there will be taking place in this vao
 	glBindVertexArray(VAO);
 	 
+		glGenBuffers(1, &IBO);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 		//Inside the VAO, we are creating the VBO and it gives the id and we take it with the VBO
 		glGenBuffers(1, &VBO);
 		//We want to bind the id of the vbo
@@ -88,7 +101,11 @@ void CreateTriangle()
 
 		//unbinding
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
+	
 	glBindVertexArray(0);
+
+	//we should unbind the ibo buffer after unbinding the vao
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 }
 
 void AddShader(GLuint theProgram, const char* shaderCode, GLenum shaderType)
@@ -153,6 +170,7 @@ void CompileShaders()
 	}
 
 	uniformModel = glGetUniformLocation(shader, "model");
+	uniformProjection = glGetUniformLocation(shader, "projection");
 }
 
 int main()
@@ -202,12 +220,18 @@ int main()
 		return 1;
 	}
 
+	glEnable(GL_DEPTH_TEST);
+
 	//Setup viewport size
 	glViewport(0, 0, bufferWidth, bufferHeight);
 
 	CreateTriangle();
 	CompileShaders();
 
+	//unifrom value setted once
+	glm::mat4 projection = glm::perspective(45.0f, (GLfloat)bufferWidth / bufferHeight, 0.1f, 100.0f);
+
+	
 	//Loop until window closed
 	while (!glfwWindowShouldClose(mainWindow))
 	{
@@ -228,10 +252,10 @@ int main()
 			direction = !direction;
 		}
 
-		curAngle += 1.0f;
-		if (curAngle > 360)
+		curAngle += 0.3f;
+		//if (curAngle > 360)
 		{
-			curAngle = 0.0f;
+			//curAngle = 0.0f;
 		}
 
 		if (direction)
@@ -250,20 +274,24 @@ int main()
 
 		//Clear window
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glUseProgram(shader);
 
 		glm::mat4 model(1.0f);
-		//model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
-		//model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
-		model = glm::scale(model, glm::vec3(curSize, 0.4f, 1.0f));
+		model = glm::translate(model, glm::vec3(0.0f, triOffset, -2.0f));
+		model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 0.7f));
 
 		//setting model matrix inside the shader
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		//setting projection matrix 
+		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
 
 			glBindVertexArray(VAO);
-				glDrawArrays(GL_TRIANGLES, 0, 3);
+				glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+				glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
+				glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 			glBindVertexArray(0);
 
 		glUseProgram(0);
