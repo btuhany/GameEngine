6e7b2d0ef8a05b59a11f70b270282bfd6e3c7fd8
btuhany <114235184+btuhany@users.noreply.github.com>
2024-06-16 21:19:17 +0300

    +stb_image directory change

--- OpenGLGameEngine/GameEngine/src/GameEngine/Core/Core.h
+++ OpenGLGameEngine/GameEngine/src/GameEngine/Core/Core.h
@@ -1,11 +1,16 @@
 #pragma once
 
+
 #ifdef GAME_ENGINE_WINDOWS
-	#ifdef GAME_ENGINE_BUILD_DLL
-		#define ENGINE_API __declspec(dllexport)
+	#ifdef GAME_ENGINE_USE_DYNAMIC_LIB
+		#ifdef GAME_ENGINE_BUILD_DLL
+			#define ENGINE_API __declspec(dllexport)
+		#else
+			#define ENGINE_API __declspec(dllimport)
+		#endif // GAME_ENGINE_BUILD_DLL
 	#else
-		#define ENGINE_API __declspec(dllimport)
-	#endif // GAME_ENGINE_BUILD_DLL
+		#define ENGINE_API
+	#endif // GAME_ENGINE_USE_DYNAMIC_LIB
 #else
 	#error This game engine supports only WINDOWS!
 #endif // GAME_ENGINE_WINDOWS
