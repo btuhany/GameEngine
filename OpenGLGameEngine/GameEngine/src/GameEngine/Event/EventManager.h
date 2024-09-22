28d1e54fc47a5a134925c44a91f6e3aa50d0eabc
btuhany <114235184+btuhany@users.noreply.github.com>
2024-09-22 17:18:42 +0300

    +listener and unsubscribe checks

--- OpenGLGameEngine/GameEngine/src/GameEngine/Event/EventManager.h
+++ OpenGLGameEngine/GameEngine/src/GameEngine/Event/EventManager.h
@@ -5,6 +5,7 @@
 #include <iostream>
 #include "../Core.h"
 #include "Event.h"
+#include "../Debug/Log.h"
 
 namespace GameEngine
 {
@@ -33,19 +34,38 @@ namespace GameEngine
 			const std::type_index eventType = EventType().GetType();
 			auto& eventListeners = m_Listeners[eventType];
 
-			eventListeners.erase(std::remove_if(eventListeners.begin(), eventListeners.end(),
+			//eventListeners.erase(std::remove_if(eventListeners.begin(), eventListeners.end(),
+			//	[&callback](const Listener& listener) {
+			//		return listener.Callback.target<void(std::shared_ptr<EventType>)>() == callback.target<void(std::shared_ptr<EventType>)>();
+			//	}),
+			//	eventListeners.end());
+
+			auto it = std::find_if(eventListeners.begin(), eventListeners.end(),
 				[&callback](const Listener& listener) {
 					return listener.Callback.target<void(std::shared_ptr<EventType>)>() == callback.target<void(std::shared_ptr<EventType>)>();
-				}),
-				eventListeners.end());
+				});
+
+			if (it == eventListeners.end()) {
+				LOG_CORE_WARN("No listener found");
+				return;
+			}
+
+
+			eventListeners.erase(it);
 		}
 
 		void Publish(std::shared_ptr<IEvent> event) {
 			const std::type_index eventType = event->GetType();
-			if (m_Listeners.find(eventType) != m_Listeners.end()) {
-				for (auto& listener : m_Listeners[eventType]) {
+			if (m_Listeners.find(eventType) != m_Listeners.end()) 
+			{
+				for (auto& listener : m_Listeners[eventType]) 
+				{
 					listener.Callback(event);
 				}
+			}
+			else
+			{
+				LOG_CORE_WARN("There is no listener of the event");
 			}
 		}
 
