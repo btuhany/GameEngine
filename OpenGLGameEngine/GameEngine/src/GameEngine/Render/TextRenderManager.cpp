fe37d6fab7f1c51afea92da8306c06ea53682a3f
btuhany <114235184+btuhany@users.noreply.github.com>
2024-11-18 00:25:58 +0300

    +rendering with main shader

--- OpenGLGameEngine/GameEngine/src/GameEngine/Render/TextRenderManager.cpp
+++ OpenGLGameEngine/GameEngine/src/GameEngine/Render/TextRenderManager.cpp
@@ -2,9 +2,13 @@
 namespace GameEngine
 {
     Shader* textShader;
-    TextData* textData;
-	void TextRenderManager::Initialize()
+    MeshData* textData;
+    GLuint VAO, VBO, EBO;
+    std::shared_ptr<SpriteRenderData> breakoutSpriteRenderData;
+    Scene* scenee;
+	void TextRenderManager::Initialize(std::shared_ptr<Shader> mainShader, Scene* scen2e)
 	{
+        scenee = scen2e;
 		FT_Library ft;
 		if (FT_Init_FreeType(&ft))
 		{
@@ -19,7 +23,7 @@ namespace GameEngine
 			return;
 		}
 
-		FT_Set_Pixel_Sizes(face, 0, 150);  //0 means dynamically calculate the width based on height
+		FT_Set_Pixel_Sizes(face, 0, 48);  //0 means dynamically calculate the width based on height
 
 
 		if (FT_Load_Char(face, 'X', FT_LOAD_RENDER))
@@ -76,60 +80,144 @@ namespace GameEngine
         textShader = new Shader();
         textShader->CreateFromFiles(vShaderLocation, fShaderLocation);
 
-        textData = new TextData();
-        textData->CreateMesh();
+        glGenVertexArrays(1, &VAO);
+        glBindVertexArray(VAO);
+
+        // Generate and bind Vertex Buffer Object (VBO)
+        glGenBuffers(1, &VBO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+        // Define vertex attributes (position and texture coordinates)
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+        glEnableVertexAttribArray(1);
+
+        // Generate and bind Element Buffer Object (EBO)
+        glGenBuffers(1, &EBO);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
+        // Unbind VAO (to avoid accidental modification)
+        glBindVertexArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+
+        std::shared_ptr<Texture> blueTex = std::make_shared<Texture>("src/BreakoutGame/Textures/01-Breakout-Tiles.PNG");
+        blueTex->LoadTextureWithAlpha();
+        breakoutSpriteRenderData = std::make_shared<SpriteRenderData>(blueTex, nullptr, mainShader);
 	}
     void TextRenderManager::Render()
     {
-        std::string text = "HELLO HELLO HELLO HELLO HELLO HELLO HELLO HELLO HELLO HELLO HELLO HELLO HELLO HELLO HELLO HELLO HELLO HELLO ";
-        auto VAO = textData->GetVAO();
-        auto VBO = textData->GetVBO();
-        auto color = glm::vec3(0.0f, 1.0f, 0.0f);
-        float scale = 1.0f;
-        float x = 300.0f;
-        float y = 300.0f;
+        std::string text = "TOOK ME A DAY";
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        glViewport(0, 0, 1920, 1080);
+        //Clear window
+        //glClearColor(0.0f, 0.5f, 0.0f, 1.0f);
+        //glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        auto mainShader = breakoutSpriteRenderData->shader;
+        mainShader->UseShader();
+        glUniformMatrix4fv(mainShader->GetViewLocation(), 1, GL_FALSE, glm::value_ptr(scenee->getCamera()->CalculateViewMatrix()));
+        glUniformMatrix4fv(mainShader->GetModelLocation(), 1,
+            GL_FALSE, glm::value_ptr(
+                glm::translate(glm::mat4(1.0f), glm::vec3(1.0f, 1.0f, 1.0f)) * 
+                glm::mat4(1.0f) *
+                glm::scale(glm::mat4(1.0f), glm::vec3(1.0f, 1.0f, 1.0f))
+            ));
+        glUniformMatrix4fv(mainShader->GetProjectionLocation(), 1, GL_FALSE, glm::value_ptr(scenee->getCamera()->CalcGetProjectionMatrix(1080.0f / 1920.0f)));
+        if (breakoutSpriteRenderData->material != nullptr)
+        {
+            breakoutSpriteRenderData->material->UseMaterial(breakoutSpriteRenderData->shader->GetMatSpecularIntensityLocation(), breakoutSpriteRenderData->shader->GetMatShininessLocation());
+        }
 
-        glEnable(GL_BLEND);
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-        glm::mat4 projection = glm::ortho(0.0f, 800.0f, -10.0f, 600.0f);
+        if (breakoutSpriteRenderData->texture != nullptr)
+        {
+            breakoutSpriteRenderData->texture->UseTexture();
+        }
 
-        textShader->UseShader();
-        glUniform3f(glGetUniformLocation(textShader->shaderID, "textColor"), color.x, color.y, color.z);
-        glActiveTexture(GL_TEXTURE0);
+        auto spriteMeshData = breakoutSpriteRenderData->quadMesh;
+        glBindVertexArray(spriteMeshData->GetVAO());
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, spriteMeshData->GetIBO());
+        glDrawElements(GL_TRIANGLES, spriteMeshData->GetIndexCount(), GL_UNSIGNED_INT, 0);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
+
+        glm::mat4 projectionMatrix = glm::ortho(-500.0f, 500.0f, -500.0f, 500.0f, -500.0f, 500.0f);
+        glUniformMatrix4fv(textShader->GetProjectionLocation(), 1, GL_FALSE, glm::value_ptr(projectionMatrix));
+        auto hh = glGetUniformLocation(textShader->shaderID, "textColor");
+        glActiveTexture(GL_TEXTURE2);
+        glUniform3f(hh, 0.5f, 1.0f, 0.5f);
         glBindVertexArray(VAO);
 
+        std::vector<float> vertices;
+        std::vector<unsigned int> indices;
+
+        unsigned int indexOffset = 0;
+        float x = -500.0f;
+        float y = 0.0f;
+        // Iterate through all characters and generate vertex and index data
         std::string::const_iterator c;
         for (c = text.begin(); c != text.end(); c++)
         {
             TextCharacter ch = charactersMap[*c];
-
-            float xpos = x + ch.bearing.x * scale;
-            float ypos = y - (ch.size.y - ch.bearing.y) * scale;
-
-            float w = ch.size.x * scale;
-            float h = ch.size.y * scale;
-            // update VBO for each character
-            float vertices[6][4] = {
+            float w = 30.0f;
+            float h = 30.0f;
+            float xpos = x;
+            float ypos = y;
+            // Define the vertices for the current character
+            float charVertices[4][4] = {
                 { xpos,     ypos + h,   0.0f, 0.0f },
                 { xpos,     ypos,       0.0f, 1.0f },
                 { xpos + w, ypos,       1.0f, 1.0f },
-
-                { xpos,     ypos + h,   0.0f, 0.0f },
-                { xpos + w, ypos,       1.0f, 1.0f },
                 { xpos + w, ypos + h,   1.0f, 0.0f }
             };
-            // render glyph texture over quad
-            glBindTexture(GL_TEXTURE_2D, ch.textureID);
-            // update content of VBO memory
-            glBindBuffer(GL_ARRAY_BUFFER, VBO);
-            glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
-            glBindBuffer(GL_ARRAY_BUFFER, 0);
-            // render quad
-            glDrawArrays(GL_TRIANGLES, 0, 6);
-            // now advance cursors for next glyph (note that advance is number of 1/64 pixels)
-            x += (ch.advance >> 6) * scale; // bitshift by 6 to get value in pixels (2^6 = 64)
+
+            // Append vertices for this character to the main vertex vector
+            for (int i = 0; i < 4; ++i)
+            {
+                vertices.insert(vertices.end(), { charVertices[i][0], charVertices[i][1], charVertices[i][2], charVertices[i][3] });
+            }
+
+            // Define the indices for the current character quad (two triangles)
+            indices.insert(indices.end(), {
+                indexOffset, indexOffset + 1, indexOffset + 2,
+                indexOffset, indexOffset + 2, indexOffset + 3
+                });
+
+            // Update index offset for the next character
+            indexOffset += 4;
+
+            // Advance the cursor for the next glyph (advance is in 1/64 pixels)
+            x += 40.0f;
+        
+
+        // Update VBO with the complete vertex data for the string
+        glBindTexture(GL_TEXTURE_2D, ch.textureID);
+
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_DYNAMIC_DRAW);
+
+
+        // Update EBO with the index data
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_DYNAMIC_DRAW);
+
+        // Render all characters in one draw call using indices
+        glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(indices.size()), GL_UNSIGNED_INT, 0);
+
+        // Clean up
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
         }
         glBindVertexArray(0);
         glBindTexture(GL_TEXTURE_2D, 0);
+
+
+        GLenum err = glGetError();
+        if (err != GL_NO_ERROR)
+        {
+            // Hata mesajýný yazdýrma
+            std::cerr << "OpenGL Error: " << err << std::endl;
+        }
     }
 }
