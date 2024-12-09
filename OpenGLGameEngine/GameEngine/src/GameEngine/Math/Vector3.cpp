7b02f7b3732f38df312f64a463a129ad175a201f
btuhany <114235184+btuhany@users.noreply.github.com>
2024-12-09 23:54:15 +0300

    +static cons values vector3

--- OpenGLGameEngine/GameEngine/src/GameEngine/Math/Vector3.cpp
+++ OpenGLGameEngine/GameEngine/src/GameEngine/Math/Vector3.cpp
@@ -39,4 +39,13 @@ namespace GameEngine
         float mag = magnitude();
         return Vector3(x / mag, y / mag, z / mag);
     }
+
+    const Vector3 Vector3::up(0.0f, 1.0f, 0.0f);
+    const Vector3 Vector3::down(0.0f, -1.0f, 0.0f);
+    const Vector3 Vector3::left(-1.0f, 0.0f, 0.0f);
+    const Vector3 Vector3::right(1.0f, 0.0f, 0.0f);
+    const Vector3 Vector3::zero(0.0f, 0.0f, 0.0f);
+    const Vector3 Vector3::forward(0.0f, 0.0f, 1.0f);
+    const Vector3 Vector3::back(0.0f, 0.0f, -1.0f);
+
 }
