daca813c92898053086445f9ab00453295d74971
btuhany <114235184+btuhany@users.noreply.github.com>
2024-12-16 22:40:16 +0300

    +able to collide check

--- OpenGLGameEngine/GameEngine/src/GameEngine/Collision/CollisionManager.cpp
+++ OpenGLGameEngine/GameEngine/src/GameEngine/Collision/CollisionManager.cpp
@@ -28,12 +28,18 @@ namespace GameEngine
 
 		for (size_t i = 0; i < m_ColliderComponents.size(); i++)
 		{
+			if (!isAbleToCollide(m_ColliderComponents[i]))
+				continue;
+
 			auto controlledCollider = m_ColliderComponents[i];
 			if (controlledCollider->getCollisionType() != CollisionType::Dynamic)
 				continue;
 
 			for (size_t j = 0; j < m_ColliderComponents.size(); j++)
 			{
+				if (!isAbleToCollide(m_ColliderComponents[j]))
+					continue;
+
 				if (i == j)
 					continue;
 
@@ -107,6 +113,14 @@ namespace GameEngine
 				}
 			}
 		}
+	}
+	bool CollisionManager::isAbleToCollide(std::shared_ptr<ColliderComponent> collider)
+	{
+		if (collider->getEntity().expired())
+			return false;
+
+		auto ownerEntity = collider->getEntity().lock();
+		return ownerEntity->IsActive() && collider->getEnabled();
 	}
 	std::shared_ptr<CollisionData> CollisionManager::AreBoxCollidersCollide(std::shared_ptr<BoxCollider2DComponent> controlledCollider, std::shared_ptr<BoxCollider2DComponent> otherCollider)
 	{
